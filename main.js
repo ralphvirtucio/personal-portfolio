5f57eb20b4d6d655e7421b4b64441766b0ac351a
ralphvirtucio <ralphvirtucio3@gmail.com>
2023-12-13 18:36:46 +0800

    personal-portfolio-version1

---
+++ main.js
@@ -0,0 +1,9 @@
+const navigation = document.querySelector('.website__nav');
+
+if (navigation.textContent === 'About Me') {
+  navigation.style.textDecoration = 'underline';
+} else if (navigation.textContent === 'Projects') {
+  navigation.style.textDecoration = 'underline';
+} else if (navigation.textContent === 'Contact') {
+  navigation.style.textDecoration = 'underline';
+}
